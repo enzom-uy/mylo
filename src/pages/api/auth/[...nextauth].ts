a8baa7ae1c5f750a0a4cdfeba87d73520c18c86c
enzom-uy <enzom.dev@gmail.com>
2022-08-17 20:46:22 -0300

    lol

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,18 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const prisma = new PrismaClient();
+
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return Promise.resolve("/account");
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
